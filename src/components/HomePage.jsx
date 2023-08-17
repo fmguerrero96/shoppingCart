bc92b5a2c0ba67592ce487ce87d790000d355662
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-17 13:11:11 -0400

    Create HomePage component and determine its content

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,16 @@
+export default function HomePage() {
+    return(
+        <>
+            <NavBar />
+            <h1>
+                Welcome to FakeFashio
+            </h1>
+            <p>
+            Discover the latest trends in mock fashion at FakeFashion! 
+            Explore our wide range of non-existent clothing, shoes, and accessories. 
+            Whether you're looking for the perfect imaginary outfit or simply browsing 
+            for inspiration, you're in for a virtual shopping experience like no other.
+            </p>
+        </>
+    )
+}
