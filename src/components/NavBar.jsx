f56e69448aa83b4ceca4ea3d68b0ffaa81fcce0f
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-17 13:00:37 -0400

    Determine nav bar content

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+export default function NavBar() {
+    return (
+    <>
+        <h1>
+            FakeFashion
+        </h1>
+        <div className="pageLinks">
+            <div>Home</div>
+            <div>Shoop</div>
+        </div>
+        <div>Cart</div>
+    </>
+    )
+}
