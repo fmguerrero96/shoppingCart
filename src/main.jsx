2575ace0923f9d101c3537fd807ffb7dd7c3ede5
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-19 20:59:06 -0400

    Import createBrowserRouter and RouterProvider, and create the configuration for a router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from './components/HomePage.jsx';
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path:"/home",
+    element: <HomePage />
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
