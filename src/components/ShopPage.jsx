c07ccf332ffd0d06ff748cdc365d29a359b4be53
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-19 20:27:16 -0400

    Create ShopPage component and determine its content

---
+++ src/components/ShopPage.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar.jsx/"
+
+export default function ShopPage() {
+    return(
+        <div className="shopPage">
+            <NavBar />
+            <h2>Shop</h2>
+            <div className="productsContainer">
+                
+            </div>
+        </div>
+    )
+}
