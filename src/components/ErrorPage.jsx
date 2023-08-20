405c34ffa6d6f986c1f714ef01f0387e7502eefc
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-19 21:51:31 -0400

    Create ErrorPage component

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import NavBar from "./navBar";
 
 const ErrorPage = () => {
   return (
     <div>
-      <h1>PAGE NOT FOUND</h1>
-      <Link to="/">
+        <NavBar />
+        <h1>PAGE NOT FOUND</h1>
+        <Link to="/">
         Click here to go back to the home page.
-      </Link>
+        </Link>
     </div>
   );
 };
