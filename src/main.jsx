f417210e10f5c8d968d10e1a691ec466099fbae9
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-19 21:52:13 -0400

    Include error element in router configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ShopPage from './components/ShopPage.jsx';
+import ErrorPage from './components/ErrorPage.jsx';
 
 const router = createBrowserRouter ([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
     path: '/shop',
