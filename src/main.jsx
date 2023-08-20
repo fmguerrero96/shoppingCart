103ae25d028d9075f4b51c09d3f9f263b54e9402
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-19 22:03:20 -0400

    Delete unnecessary code and import Router component

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ShopPage from './components/ShopPage.jsx';
-import ErrorPage from './components/ErrorPage.jsx';
-
-const router = createBrowserRouter ([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/shop',
-    element: <ShopPage />
-  },
-])
-
+import Router from './components/Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router />
   </React.StrictMode>,
 )
