8d3c504dcd71551cc8b80b95102d6b04a9f5852d
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-19 21:53:53 -0400

    Add asterisks to error message

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
   return (
     <div>
         <NavBar />
-        <h1>PAGE NOT FOUND</h1>
+        <h1>*PAGE NOT FOUND*</h1>
         <Link to="/">
         Click here to go back to the home page.
         </Link>
