b3c63660d50203142bb27cc4bad0ed9fe344e465
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-21 11:34:24 -0400

    Shorten description message

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -11,8 +11,7 @@ export default function HomePage() {
                 <p className="description">
                 Discover the latest trends in mock fashion at FakeFashion! 
                 Explore our wide range of non-existent clothing, shoes, and accessories. 
-                Whether you're looking for the perfect imaginary outfit or simply browsing 
-                for inspiration, you're in for a virtual shopping experience like no other.
+                
                 </p>
             </div>
         </div>
