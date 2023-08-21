a2e12d5865e8f96da6f2e9c2ec6c1dfb8a23300b
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-21 09:51:34 -0400

    Add more class names to nav bar elements

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 export default function NavBar() {
     return (
     <div className='navBar'>
-        <h1>
+        <h1 className='navTitle'>
             FakeFashion
         </h1>
         <div className="pageLinks">
-            <Link to='/'>Home</Link>
-            <Link to='/shop'>Shop</Link>
+            <Link className='homeLink' to='/'>Home</Link>
+            <Link className='shopLink' to='/shop'>Shop</Link>
         </div>
-        <div>Cart</div>
+        <div className='cartIcon'>Cart</div>
     </div>
     )
 }
