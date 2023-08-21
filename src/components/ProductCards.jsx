289ade9d05778092efcb32189fce90fbaa1123a8
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-20 21:54:37 -0400

    Determine the contents of each product card

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -11,7 +11,17 @@ export default function ProductCards() {
 
     return(
         <div className="productsContainer">
-            
+            {products ? (
+                products.map((product) =>(
+                    <div className="productCard" key={product.id}>
+                        <img src={product.image} alt={product.description}></img>
+                        <p>{product.title}</p>
+                        <p>{product.price}</p>
+                    </div>
+                ))
+            ) : (
+                <p>Loading products...</p>
+            )}
         </div>
     )
 }
