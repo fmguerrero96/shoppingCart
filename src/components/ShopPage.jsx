e0084efcf684b95475cfb3e23c5d67a580ccc79f
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-20 21:52:09 -0400

    Import ProductCards and remove unnecessary element

--- src/components/ShopPage.jsx
+++ src/components/ShopPage.jsx
@@ -1,13 +1,13 @@
 import NavBar from "./NavBar.jsx/"
+import ProductCards from "./ProductCards"
 
 export default function ShopPage() {
+    
     return(
         <div className="shopPage">
             <NavBar />
             <h2>Shop</h2>
-            <div className="productsContainer">
-                
-            </div>
+            <ProductCards/>
         </div>
     )
 }
