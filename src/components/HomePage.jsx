528dcf9a4b9af5fe5b8075c505d8b64480afb9ba
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-21 10:17:35 -0400

    Add new div for home content section

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,15 +4,17 @@ export default function HomePage() {
     return(
         <div className="homePage">
             <NavBar />
-            <h1>
-                Welcome to FakeFashion
-            </h1>
-            <p>
-            Discover the latest trends in mock fashion at FakeFashion! 
-            Explore our wide range of non-existent clothing, shoes, and accessories. 
-            Whether you're looking for the perfect imaginary outfit or simply browsing 
-            for inspiration, you're in for a virtual shopping experience like no other.
-            </p>
+            <div className="homeContent">
+                <h1>
+                    Welcome to FakeFashion
+                </h1>
+                <p>
+                Discover the latest trends in mock fashion at FakeFashion! 
+                Explore our wide range of non-existent clothing, shoes, and accessories. 
+                Whether you're looking for the perfect imaginary outfit or simply browsing 
+                for inspiration, you're in for a virtual shopping experience like no other.
+                </p>
+            </div>
         </div>
     )
 }
