bed325faca467fa3661bf45fcc8731d3d4c696c8
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-23 18:25:37 -0400

    Remove state and click function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
 import Router from "./components/Router";
-import { useState } from "react"
 
 function App() {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-
-  function handleCartToggle() {
-    setIsCartOpen(!isCartOpen)
-    console.log(isCartOpen)
-  }
-
   return (
     <>
       <Router onCartToggle={handleCartToggle} isOpen={isCartOpen} onCartClose={handleCartToggle} />
