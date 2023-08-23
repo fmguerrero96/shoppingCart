fc9d65c26995f896ba700f4807636027247bf70b
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-23 17:39:17 -0400

    Create function to open and close cart, and pass props into HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import HomePage from "./components/HomePage"
+//import Cart from "./components/Cart"
+//import NavBar from "./components/navBar"
+import { useState } from "react"
 
 function App() {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  function handleCartToggle() {
+    setIsCartOpen(!isCartOpen)
+    console.log(isCartOpen)
+  }
+
   return (
     <>
-    <HomePage />
+      <HomePage onCartToggle={handleCartToggle} isOpen={isCartOpen} onCartClose={handleCartToggle} />
     </>
   )
 }
