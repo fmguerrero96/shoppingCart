dae0e32ba502b0578c8e7c9c1553663db66a248d
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-23 18:46:10 -0400

    Remove props

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Router from "./components/Router";
 function App() {
   return (
     <>
-      <Router onCartToggle={handleCartToggle} isOpen={isCartOpen} onCartClose={handleCartToggle} />
+      <Router />
     </>
   )
 }
