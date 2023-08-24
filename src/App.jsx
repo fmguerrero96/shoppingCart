8edb41d41119c04438c84527351ccbe029848007
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-24 10:01:11 -0400

    Create isCartOpen state variable and toggle function to open the cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import Router from "./components/Router";
 
 function App() {
+  const [isCartOpen, setIsCartOpen] = useState(false)
+
+  function handleCartToggle() {
+    setIsCartOpen(!isCartOpen)
+    console.log(isCartOpen)
+  }
+
   return (
     <>
-      <Router />
+      <Router isOpen={isCartOpen} onCartToggle={handleCartToggle} />
     </>
   )
 }
