26c2d447e9ad167b1adb878c68815263203b09b6
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-24 11:25:28 -0400

    Move state variable and toggle function into Router component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,6 @@ import { useState } from "react";
 import Router from "./components/Router";
 
 function App() {
-  const [isCartOpen, setIsCartOpen] = useState(false)
-
-  function handleCartToggle() {
-    setIsCartOpen(!isCartOpen)
-    console.log(isCartOpen)
-  }
-
   return (
     <>
       <Router isOpen={isCartOpen} onCartToggle={handleCartToggle} />
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,17 +2,24 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ShopPage from "./ShopPage.jsx";
 import ErrorPage from "./ErrorPage.jsx"
 import HomePage from "./HomePage.jsx";
+import { useState } from "react";
 
-export default function Router({isOpen, onCartToggle}) {
+export default function Router() {
+  const [isCartOpen, setIsCartOpen] = useState(false)
+
+  function handleCartToggle() {
+    setIsCartOpen(!isCartOpen)
+    console.log(isCartOpen)
+  }
     const router = createBrowserRouter ([
         {
           path: "/",
-          element: <HomePage onCartToggle={onCartToggle} isOpen={isOpen} navBar={navBar}/>,
+          element: <HomePage isOpen={isCartOpen} onCartToggle={handleCartToggle}/>,
           errorElement: <ErrorPage />,
         },
         {
           path: '/shop',
-          element: <ShopPage onCartToggle={onCartToggle} isOpen={isOpen} navBar={navBar}/>
+          element: <ShopPage isOpen={isCartOpen} onCartToggle={handleCartToggle}/>
         },
       ])
 
