d701ec2089fc9038683cf6b44a61332b3b67305d
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-27 21:01:24 -0400

    Add 'each' className

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -7,7 +7,7 @@ export default function CartItem({cart}) {
                         <div className="cartProduct" key={item.id}>
                             <div className="imagePrice">
                                 <img className="itemImage" src={item.image} alt={item.description}></img>
-                                <div>${item.price} each</div>
+                                <div className="each">${item.price} each</div>
                             </div>
                             
                             <div className="amountControl">
