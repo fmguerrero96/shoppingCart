b574abc6ce9d9f6a7330716c83307d32114f6926
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-27 20:30:12 -0400

    Change structure of CartItem component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -5,11 +5,16 @@ export default function CartItem({cart}) {
                 cart.length > 0  ? (
                     cart.map((item) => (
                         <div className="cartProduct">
-                            <img className="itemImage" src={item.image} alt={item.description}></img>
-                            <span>${item.price} each</span>
-                            <button className="subtract">-</button>
-                            <div className="amount">0</div>
-                            <button className="add">+</button>
+                            <div className="imagePrice">
+                                <img className="itemImage" src={item.image} alt={item.description}></img>
+                                <div>${item.price} each</div>
+                            </div>
+                            
+                            <div className="amountControl">
+                                <button className="subtract">-</button>
+                                <div className="amount">0</div>
+                                <button className="add">+</button>
+                            </div>
                         </div>
                     ))
                 ) : (
