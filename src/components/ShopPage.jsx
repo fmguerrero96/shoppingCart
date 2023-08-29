14e30733e1768ac02990beee0a618da9bf4a38b8
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-29 15:47:42 -0400

    Add 'shopTitle' class name and style shopTitle element

--- src/components/ShopPage.jsx
+++ src/components/ShopPage.jsx
@@ -6,7 +6,7 @@ export default function ShopPage({isOpen, onCartToggle, products, onAddToCart, c
     return(
         <div className="shopPage">
             <NavBar total={total} onDeleteItem={onDeleteItem} onQuantityChange={onQuantityChange} cart={cart} isOpen={isOpen} onCartToggle={onCartToggle}/>
-            <h2>Shop</h2>
+            <h2 className="shopTitle">Shop</h2>
             <ProductCards onAddToCart={onAddToCart} products={products}/>
         </div>
     )
