279d0aac2928294694e820669b7f9342ad1675c9
Misael Guerrero <mguerrerowest@gmail.com>
2023-08-30 13:37:13 -0400

    Remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
 import Router from "./components/Router";
 
 function App() {
   return (
     <>
-      <Router isOpen={isCartOpen} onCartToggle={handleCartToggle} />
+      <Router />
     </>
   )
 }
