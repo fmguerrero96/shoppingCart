7613c0ccb7b684662cb6d3e9672a9887b71c7730
Misael Guerrero <mguerrerowest@gmail.com>
2023-09-01 10:27:06 -0400

    Add a third test

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from '../src/App';
 import '@testing-library/jest-dom'
+import { expect } from 'vitest';
 
 describe("App component", () => {
   it("renders the HomePage component", () => {
@@ -11,7 +12,13 @@ describe("App component", () => {
 
   it("renders the HomePage component", () => {
     render(<App />);
-    const homePageElement = screen.getByTestId("home-page"); // Assuming you set a test ID for HomePage
+    const homePageElement = screen.getByTestId("home-page"); 
     expect(homePageElement).toBeInTheDocument();
   });
+
+  it("renders the NavBar component", () => {
+    render(<App />);
+    const navBar = screen.getByTestId("navBar");
+    expect(navBar).toBeInTheDocument()
+  })
 });
