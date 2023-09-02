406d2deb5f02c23e16cd7aad71e6f6fd85cc69d8
Misael Guerrero <mguerrerowest@gmail.com>
2023-09-02 18:49:34 -0400

    Write second test of NavBar component

--- tests/NavBar.test.jsx
+++ tests/NavBar.test.jsx
@@ -2,15 +2,33 @@ import { render, screen } from '@testing-library/react';
 import NavBar from '../src/components/navBar';
 import '@testing-library/jest-dom'
 import { expect } from 'vitest';
-import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import { BrowserRouter } from "react-router-dom"; 
+import userEvent from "@testing-library/user-event";
+
 
 describe("NavBar component", () => {
-it("renders NavBar compoent", () => {
-    const sampleCart = ['helo']
-    render(<BrowserRouter>
-        <NavBar cart={sampleCart} />
-      </BrowserRouter>);
-    const navBarCompoent = screen.getByTestId("navBar"); 
-    expect(navBarCompoent).toBeInTheDocument();
+    it("renders NavBar compoent", () => {
+        const sampleCart = ['helo']
+        render(
+            <BrowserRouter>
+                <NavBar cart={sampleCart} />
+            </BrowserRouter>
+        );
+        const navBarComponent = screen.getByTestId("navBar"); 
+        expect(navBarComponent).toBeInTheDocument();
+        });
+    
+    it("should call the onClick function when clicked", async () => {
+        const onClick = vi.fn();
+        const sampleCart = ['helo']
+        const user = userEvent.setup()
+        render(
+            <BrowserRouter>
+                <NavBar cart={sampleCart} onCartToggle={onClick} />
+            </BrowserRouter>
+        );
+        const cartIcon = screen.getByText(/Cart/i);
+        await user.click(cartIcon);
+        expect(onClick).toHaveBeenCalled();
     });
 });
