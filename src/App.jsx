1f2e24e72e75984e111c2371f28dd7eb673a3787
Misael Guerrero <mguerrerowest@gmail.com>
2023-09-02 18:26:25 -0400

    Undo previous changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Router from "./components/Router";
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Router />
-    </BrowserRouter>
+    </>
   )
 }
 
