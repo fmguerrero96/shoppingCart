cb28838bbf8f663e01809ff87884a78dc7017a80
Misael Guerrero <mguerrerowest@gmail.com>
2023-09-03 21:14:32 -0400

    Write test to make sure cart displays correct total

--- tests/Cart.test.jsx
+++ tests/Cart.test.jsx
@@ -3,7 +3,6 @@ import Cart from '../src/components/Cart';
 import '@testing-library/jest-dom'
 import { describe, expect } from 'vitest';
 import { BrowserRouter } from "react-router-dom"; 
-import userEvent from "@testing-library/user-event";
 
 describe("Cart component", () => {
     it("renders Cart compoent", () => {
@@ -16,4 +15,22 @@ describe("Cart component", () => {
         const CartComponent = screen.getByTestId("cart"); 
         expect(CartComponent).toBeInTheDocument();
         });
+    
+    it("Should display correct total", () => {
+        const sampleCart = [{category: "men's clothing",
+            description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+            id: 1,
+            image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+            price: '109.95',
+            rating: {rate: 3.9, count: 120},
+            title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"}];
+        const total = sampleCart[0].price
+        render(
+            <BrowserRouter>
+                <Cart cart={sampleCart} total={total} />
+            </BrowserRouter>
+        );
+        const totalElement = screen.getByTestId('total').textContent
+        expect(totalElement).toEqual(total)
+    });
 });
